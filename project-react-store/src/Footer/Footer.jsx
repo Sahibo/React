574b82cc59372f3cd92b671e109f9b3739b0cdaa
Sahib Aliyev <sahib.aliyev.2002@gmail.com>
2023-10-21 17:44:53 +0400

    Catalog grid done

--- project-react-store/src/Footer/Footer.jsx
+++ project-react-store/src/Footer/Footer.jsx
@@ -1,7 +1,27 @@
+import "./styles/Footer.css";
+
 export function Footer() {
   return (
-    <footer class="text-center text-lg-start bg-black text-muted">
-     
+    <footer class="footer-container">
+      <div className="footer-column">
+        <div className="footer-title">
+          <h3>Head office</h3>
+        </div>
+
+        <div className="footer-content">
+          <p className="content-p">
+            Furni AZ MMC<br/>
+            1<sup>st</sup> Floor Cloud Mall<br/>
+            Freedom Road<br/>
+            Hazel Grove<br/>
+            Stoicport<br/>
+            XK7 2FW
+          </p>
+          <p>
+            
+          </p>
+        </div>
+      </div> 
     </footer>
   );
 }
--- project-react-store/src/Main/Catalog.jsx
+++ project-react-store/src/Main/Catalog.jsx
@@ -1,20 +1,53 @@
-import { useEffect, useState  } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchContent, handleSort  } from "../store/reducer";
+import { fetchContent, handleSort } from "../store/reducer";
 import "./styles/Catalog.css";
 
 export function Catalog() {
   const dispatch = useDispatch();
   let data = useSelector((state) => state.products.categoriesArr);
-  let [sortByPrice, setSortByPrice] = useState({ increase: 'Sort by price to increase', decrease: 'Sort by price to decrease' })
-  let [sortByCategory, setSortByCategory] = useState('')
+  let [sortByPrice, setSortByPrice] = useState({
+    increase: "Sort by price to increase",
+    decrease: "Sort by price to decrease",
+  });
+  let [productsList, setProductsList] = useState([]);
+  let [selectedCategory, setSelectedCategory] = useState("Bedroom");
+  let [subcategory, setSubcategory] = useState("");
+  let [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
     dispatch(fetchContent());
-
   }, []);
 
-  console.log(data);
+  const filteredSubcategories = data.find(
+    (category) => category.name === selectedCategory
+  )?.subcategories;
+
+  const handleCategoryChange = (e, category) => {
+    e.preventDefault();
+    setSelectedCategory(category);
+    setProductsList([]);
+    setShowAll(false);
+  };
+
+  const setSelectedSubcategory = (e, subcategory) => {
+    e.preventDefault();
+    setSubcategory(subcategory);
+    setShowAll(false);
+
+    const products =
+      data
+        .find((category) => category.name === selectedCategory)
+        ?.subcategories.find((sub) => sub.name === subcategory)?.products || [];
+
+    setProductsList(products);
+  };
+
+  const showAllProducts = (e) => {
+    e.preventDefault();
+    setShowAll(true);
+  };
+
   return (
     <div className="catalog-container">
       <div className="categories-container">
@@ -22,7 +55,13 @@ export function Catalog() {
           {Array.isArray(data) && data.length > 0 ? (
             data.map((category, index) => (
               <li className="category" key={index}>
-                {category.name}
+                <a
+                  onClick={(e) => handleCategoryChange(e, category.name)}
+                  className="category"
+                  href=""
+                  key={index}>
+                  {category.name}
+                </a>
               </li>
             ))
           ) : (
@@ -31,40 +70,68 @@ export function Catalog() {
         </ul>
       </div>
 
-      <ul>
-        <select onChange={(e) => dispatch(handleSort(e.target.value))}>
-          <option value={sortByPrice.increase}>{sortByPrice.increase}</option>
-          <option value={sortByPrice.decrease}>{sortByPrice.decrease}</option>
-        </select>
+      <div className="content-container">
+        {selectedCategory && filteredSubcategories && (
+          <aside className="side-container">
+            <ul className="subcategories-list">
+              <li className="subcategory">
+                <a href="" onClick={(e) => showAllProducts(e)}>
+                  Show All
+                </a>
+              </li>
 
-        
-
-
-
-        {Array.isArray(data) && data.length > 0 ? (
-          data.map((category, categoryIndex) => (
-            <li key={categoryIndex}>
-              {category.name}
-              {category.subcategories.map((subcategory, subcategoryIndex) => (
-                <div key={subcategoryIndex}>
-                  <h3>{subcategory.name}</h3>
-                  <ul>
-                    {subcategory.products.map((product, productIndex) => (
-                      <li key={productIndex}>
-                        {product.name} - Price: {product.price}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+              {filteredSubcategories.map((subcategory, index) => (
+                <li className="subcategory">
+                  <a onClick={(e) => setSelectedSubcategory(e, subcategory.name)}
+                    href="" key={index}>
+                      {subcategory.name}
+                    </a>
+                </li>
               ))}
-            </li>
-          ))
-        ) : (
-          <p>Error</p>
+            </ul>
+          </aside>
         )}
 
-      </ul>
+        <div className="products-outer-container">
+          <div className="products-sort">
+            <span>{productsList.length} products</span>
+            <select onChange={(e) => dispatch(handleSort(e.target.value))} className="select-box">
+              <option value={sortByPrice.increase}>
+                {sortByPrice.increase}
+              </option>
+              <option value={sortByPrice.decrease}>
+                {sortByPrice.decrease}
+              </option>
+            </select>
+          </div>
 
+          <div className="products-container">
+            {showAll ? (
+              <div className="products-list">
+                  {data.map((category) =>
+                    category.subcategories.map((subcategory) =>
+                      subcategory.products.map((product, index) => (
+                        <div key={index} className="product-item">
+                          <p>Name: {product.name}</p>
+                          <p>Price: {product.price}</p>
+                        </div>
+                      ))
+                    )
+                  )}
+              </div>
+            ) : productsList.length > 0 ? (
+              <div className="products-list">
+                  {productsList.map((product, index) => (
+                    <div key={index} className="product-item">
+                      <p>Name: {product.name}</p>
+                      <p>Price: {product.price}</p>
+                    </div>
+                  ))}
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
